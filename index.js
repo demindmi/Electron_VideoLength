ecc45da375ac30c7132a89bc43cad2fd28649fcf
Demin <326009107@maple.fg.rbc.com>
2018-08-10 14:16:27 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/index.html`) //__dirname is a local vraible from node js,
+
+}); //event based programming - we listen for of app, for the readu status and do w/e arrow function does  
+
+ipcMain.on('video:submit', (event, data ) =>{  //when this trigers, start function
+    ffmpeg.ffprobe(data, (err, metadata) => {
+        mainWindow.webContents.send('video:path', metadata); //sending back
+    })
+
+});
+//electron AP                                  Main BrowserWindow
+//ipcMain.on  <------------------------------   ipcRenderer.send | main page asking something to electron
+//mainWindow.webContents.send -------------->   ipcRenderer.on   | electron responding
+
